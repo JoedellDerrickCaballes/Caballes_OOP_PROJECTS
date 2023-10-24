75a1257339cfd82465da3e1726a752f08c3aa53d
JoedellDerrickCaballes <148180009+JoedellDerrickCaballes@users.noreply.github.com>
2023-10-24 13:01:13 +0800

    Add files via upload

---
+++ Favorite_Song/FavoriteSong.java
@@ -0,0 +1,23 @@
+package Favorite_Song;
+/**
+ * Project:  Java Programming
+ * Package: Favorite_Song
+ * Filename:  FavoriteSong
+ * Created By:  NoPetsAllowed
+ * On: 2018 At: 6:59 PM
+ *
+ * Write, compile, and test a class that displays at least four lines of your favorite song.
+ * Save the class as FavoriteSong.
+ */
+public class FavoriteSong  
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Kinsa pay kuwang kinsa nang animalang gi hulat");
+				System.out.println("E diga na tanan diha ayaw na panguwat cyaw kabalaka kay wa lagi ni kurat");
+				System.out.println("Kinsa pay kuwang kinsa nang animalang gi hulat Dili ta diri bai didto ta hunat");
+				System.out.println("Padong nata lupad maong andamang masubad Dagkoti na dayon ang ga tibugol na bud");
+				System.out.println("kinsay pai kuw-ng? kinsa nang animalang gi hulat dili ta diri bai dd2 ta hunat padung nata lupad maong andamang ma subad dakoti na dayon ang ga tibugol nga buds");
+				System.out.println("cge pamutos na, kai kita lupad pag dilikado wai kurat basta kasama ang lima nga sige rag ga hi.usa basta kai sa bong mo buto kusog mo sipa");
+			}
+	}
---
+++ Favorite_Song/Student.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Student;
+public static void main (String []args);
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Enter First name, middle name, last name, suffix");
+String input = scanner.nextLine();
+
+firstName();
+middleName();
+lastName();
+suffix();
+
+public class student
+String first_name;
+String middle_name; 
+String last_name;
+String suffix;
+
+static void 
